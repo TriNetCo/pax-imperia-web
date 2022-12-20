9a0c103434e14257f682f352a184480b412df36d
TheNotary <no@email.plz>
2022-12-19 22:55:08 -0600

    Add system buffer slider

--- frontend/script/app.js
+++ frontend/script/app.js
@@ -4,14 +4,17 @@ import { GameSettings } from './gameSettings.js';
 const canvas = document.getElementById("galaxy-canvas-large");
 const regenButton = document.getElementById("galaxy-regenerate-button");
 const systemCountSlider = document.getElementById("system-count-slider");
+const systemBufferSlider = document.getElementById("system-buffer-slider");
 
 let galaxyWidgetSettings = GameSettings.galaxyWidget;
 
 systemCountSlider.value = galaxyWidgetSettings.systemCount;
+systemBufferSlider.value = galaxyWidgetSettings.systemBuffer;
 
 let galaxyWidget;
 function generateGalaxy() {
     galaxyWidgetSettings.systemCount = systemCountSlider.value;
+    galaxyWidgetSettings.systemBuffer = systemBufferSlider.value;
     galaxyWidget = new GalaxyWidget(galaxyWidgetSettings);
     galaxyWidget.beginGame(canvas);
 }
