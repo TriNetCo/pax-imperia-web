bbf855113ccbb7c5c6ab16208a8e7272e9fd5af5
TheNotary <no@email.plz>
2022-12-21 14:18:31 -0600

    encapsolates DOM code into GalaxyDomAttacher.attachDomEventsToCode

--- frontend/script/models/galaxyWidget.js
+++ frontend/script/models/galaxyWidget.js
@@ -1,10 +1,10 @@
 import { Galaxy } from './galaxy.js';
 import { GalaxyDrawer } from './galaxyDrawer.js';
+import { GalaxyDomAttacher } from './galaxyDomAttacher.js';
 
 export class GalaxyWidget {
 
     systemClickHandler;
-    mouse;
 
     constructor(gameSettings) {
         this.width = gameSettings.canvasWidth;
@@ -24,9 +24,9 @@ export class GalaxyWidget {
         this.cx = canvas.getContext("2d");
         console.log('beginGame systemCount: ' + this.systemCount)
 
-        this.attachDomEventsToCode(this.cx, this.galaxy.systems);
         this.systemNameLabel = document.getElementById("system-name");
         this.galaxyDrawer = new GalaxyDrawer({cx: this.cx, galaxy: this.galaxy, systemNameLabel: this.systemNameLabel, mouse: this.mouse});
+        this.attachDomEventsToCode(this.cx, this.galaxy.systems);
     }
 
     draw() {
@@ -34,35 +34,7 @@ export class GalaxyWidget {
     }
 
     attachDomEventsToCode(cx, systems) {
-        // We can use our function with a canvas event
-        this.addMouseMovement(cx)
-        this.addMouseClick(cx, systems)
-    }
-
-    addMouseMovement(cx) {
-        cx.canvas.addEventListener('mousemove', e => {
-            this.mouse.x = e.offsetX;
-            this.mouse.y = e.offsetY;
-        });
-    }
-
-    addMouseClick(cx, systems) {
-        cx.canvas.addEventListener('click', e => {
-            this.mouse.x = e.offsetX;
-            this.mouse.y = e.offsetY;
-            this.clickHandler(systems);
-        });
-    }
-
-    clickHandler(systems){
-        // check if we're clicking a star system
-        systems.forEach( system => {
-            if (this.galaxyDrawer.isMouseHovering(system)) {
-                const path = "/systems/" + system.id;
-                this.systemClickHandler(path);
-            }
-        });
-
+        GalaxyDomAttacher.attachDomEventsToCode(cx, systems, this.galaxyDrawer, this.systemClickHandler, this.mouse);
     }
 
 }
