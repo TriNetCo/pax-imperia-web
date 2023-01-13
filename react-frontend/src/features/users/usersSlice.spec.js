673564fd940696c7c60dfc02b492729310c202f1
TheNotary <no@email.plz>
2023-01-12 22:36:14 -0600

    fixes failing users test from users feature

---
+++ react-frontend/src/features/users/usersSlice.spec.js
@@ -0,0 +1,12 @@
+import usersReducer, {
+} from './usersSlice';
+
+describe('counter reducer', () => {
+  it('should handle initial state', () => {
+    expect(usersReducer(undefined, { type: 'unknown' })).toEqual({
+      'currentRequestId': undefined,
+      'loading': 'idle',
+      'users': [],
+    });
+  });
+});
