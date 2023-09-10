6b8fdd43caee89bb0cfe45bed4460811287a1441
savagedata <17114944+savagedata@users.noreply.github.com>
2023-09-10 13:21:34 -1000

    Refactor ship update functions

--- pax-imperia-js/script/widgets/space_view/entities/ship.js
+++ pax-imperia-js/script/widgets/space_view/entities/ship.js
@@ -15,51 +15,64 @@ export class Ship extends Entity {
         this.speed = 0.2;
         this.destinationPoint = null; // x, y, z coordinates
         this.destinationTarget = null; // 3d object
+        this.orbitTarget = null; // 3d object
         this.previousSystemId = typeof this.previousSystemId === 'undefined' ? null : this.previousSystemId
     }
 
     update (deltaTime, system) {
         if (this.destinationTarget &&
             this.destinationTarget.parentEntity.type == 'wormhole') {
-            // if ship is close enough to wormhole, move it to the next system
-            const distanceFromDest = this.object3d.position.distanceTo(this.destinationTarget.position);
-            const wormholeId = this.destinationTarget.parentEntity.id;
-            if (distanceFromDest <= this.speed) {
-                // copy ship data to wormhole system
-                this.pushData(wormholeId);
-                // delete ship from current system
-                this.delete(system);
-            }
+                this.checkAndSendThroughWormhole();
         }
         if (this.destinationTarget) {
-            const destX = this.destinationTarget.position.x;
-            const destY = this.destinationTarget.position.y;
-            let destZ = this.destinationTarget.position.z;
-            // put ship in front of stars and planets so they can be seen
-            if (['star', 'planet'].includes(this.destinationTarget.parentEntity.type)){
-                destZ += this.destinationTarget.scale.z*2;
-            }
-            this.destinationPoint = {"x": destX, "y": destY, "z": destZ};
+            this.updateToDestinationTarget();
         }
         if (this.destinationPoint) {
-            const positionVector = new THREE.Vector3().copy(this.object3d.position);
-            const destinationVector = new THREE.Vector3().copy(this.destinationPoint);
-            const distanceFromDest = positionVector.distanceTo(destinationVector);
-
-            // If the destinationPoint is within [speed] units away from this.position,
-            // then set destinationPoint to null
-            if (distanceFromDest <= this.speed) {
-                this.object3d.position.copy(destinationVector);
-                this.destinationPoint = null;
-            } else {
-                const displacementVector = destinationVector
-                    .sub(positionVector)
-                    .normalize()
-                    .multiplyScalar(this.speed, this.speed, this.speed);
-                const finalVector = positionVector.add(displacementVector);
-                this.object3d.position.copy(finalVector);
-            }
+            this.updateToDestinationPoint();
         }
+    }
+
+    checkAndSendThroughWormhole() {
+        // if ship is close enough to wormhole, move it to the next system
+        const distanceFromDest = this.object3d.position.distanceTo(this.destinationTarget.position);
+        const wormholeId = this.destinationTarget.parentEntity.id;
+        if (distanceFromDest <= this.speed) {
+            // copy ship data to wormhole system data
+            this.pushData(wormholeId);
+            // delete ship from current system
+            this.delete(system);
+        }
+    }
+
+    updateToDestinationPoint() {
+        const positionVector = new THREE.Vector3().copy(this.object3d.position);
+        const destinationVector = new THREE.Vector3().copy(this.destinationPoint);
+        const distanceFromDest = positionVector.distanceTo(destinationVector);
+
+        // If the destinationPoint is within [speed] units away from this.position,
+        // then move to destination and set destinationPoint to null
+        if (distanceFromDest <= this.speed) {
+            this.object3d.position.copy(destinationVector);
+            this.destinationPoint = null;
+        } else {
+            const displacementVector = destinationVector
+                .sub(positionVector)
+                .normalize()
+                .multiplyScalar(this.speed, this.speed, this.speed);
+            const finalVector = positionVector.add(displacementVector);
+            this.object3d.position.copy(finalVector);
+        }
+    }
+
+    updateToDestinationTarget() {
+        const destX = this.destinationTarget.position.x;
+        const destY = this.destinationTarget.position.y;
+        let destZ = this.destinationTarget.position.z;
+        // put ship in front of stars and planets so they can be seen
+        if (['star', 'planet'].includes(this.destinationTarget.parentEntity.type)){
+            destZ += this.destinationTarget.scale.z*2;
+        }
+        this.destinationPoint = {"x": destX, "y": destY, "z": destZ};
     }
 
 }
