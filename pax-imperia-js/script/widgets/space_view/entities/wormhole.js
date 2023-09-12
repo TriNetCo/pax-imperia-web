3883e6ad1399dd41a7a8d94b8c385ee9e529a0fb
savagedata <17114944+savagedata@users.noreply.github.com>
2023-09-12 18:14:50 -0500

    Add wormhole

--- pax-imperia-js/script/widgets/space_view/entities/wormhole.js
+++ pax-imperia-js/script/widgets/space_view/entities/wormhole.js
@@ -37,7 +37,8 @@ export class Wormhole extends Entity {
         // position: { x: 783, y: 378, z: 0.94 }
         this.type = 'wormhole';
         this.assetPath = this.basePath + "/assets/planets/sun.gltf";
-        this.assetThumbnailPath = this.basePath + "/assets/thumbnails/sun_thumbnail.png";
+        // this.assetPath = this.basePath + "/assets/wormholes/wormhole.png";
+        this.assetThumbnailPath = this.basePath + "/assets/thumbnails/wormhole_thumbnail.png";
         this.size = 0.3;
         this.scale = {x: this.size, y: this.size, z: this.size};
         // this.position is in Galactic View coordinates
