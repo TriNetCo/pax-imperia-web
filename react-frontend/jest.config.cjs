eef2a0a9a00370888f897b3a02bcc5fa30c29dea
TheNotary <no@email.plz>
2023-09-18 11:34:20 -0500

    IDE: fixes bug where tests wouldn't run in IDE by matching the jest dependencies with what CRA was using under the hood, plus configs were fubar

--- react-frontend/jest.config.cjs
+++ react-frontend/jest.config.cjs
@@ -5,15 +5,15 @@ const config = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tools/fileMock.js',
     '\\.(css|less)$': '<rootDir>/tools/styleMock.js'
   },
-//   testEnvironment: '<rootDir>/node_modules/jest-environment-jsdom',
-  testEnvironment: 'jsdom',
+  testEnvironment: '<rootDir>/node_modules/jest-environment-jsdom',
+//   testEnvironment: 'jsdom',
 //   transform: {},
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  displayName: 'react-frontend',
-  moduleDirectories: ['<rootDir>/node_modules', '<rootDir>'],
+//   displayName: 'react-frontend',
+//   moduleDirectories: ['<rootDir>/node_modules', '<rootDir>'],
   modulePaths: ['<rootDir>/node_modules', '<rootDir>'],
 //   modulePaths: ['<rootDir>/node_modules', '<rootDir>', '<rootDir>/../pax-imperia-js/node_modules'],
-  testMatch: ['<rootDir>/**/*.test.js']
+//   testMatch: ['<rootDir>/**/*.test.js']
 };
 
 module.exports = config;
