6bc90e6250e89d3367b974857ff21acdd27ed081
savagedata <17114944+savagedata@users.noreply.github.com>
2023-09-19 22:19:55 -0500

    changes code quality of Galaxy class

--- pax-imperia-js/script/models/galaxy.js
+++ pax-imperia-js/script/models/galaxy.js
@@ -6,35 +6,60 @@ import { System } from '../../script/widgets/space_view/entities/system.js'
 
 export class Galaxy {
 
-    constructor(galaxyWidgetSettings = null, systemsJson = ""){
-        // generate new systemsData or use given systemsJson
-        let systemsData = [];
-        if (systemsJson === "") {
-            // galaxyWidgetSettings should not be null
-            systemsData = this.generateSystems(galaxyWidgetSettings);
-            const connections = this.generateConnections(systemsData);
-            this.addConnectionsToSystems(systemsData, connections);
-        } else {
-            systemsData = JSON.parse(systemsJson);
+    /**
+     * @param {Object} galaxyWidgetSettings Configurations passed to galaxy from the galaxyWidget.  If this parameter is omitted, systemsJson must be provided.
+     * @param {number} galaxyWidgetSettings.canvasWidth Determines the area where stars can be placed
+     * @param {number} galaxyWidgetSettings.canvasHeight Determines the area where stars can be placed
+     * @param {number} galaxyWidgetSettings.canvasBuffer The padding between where the stars can be placed and the canvas perimeter
+     * @param {number} galaxyWidgetSettings.maxPlacementAttempts description
+     * @param {number} galaxyWidgetSettings.systemCount Number of systems to generate in the galaxy
+     * @param {number} galaxyWidgetSettings.systemRadius description...
+     * @param {number} galaxyWidgetSettings.systemBuffer description
+     * @param {number} galaxyWidgetSettings.minStarSize description
+     * @param {number} galaxyWidgetSettings.maxStarSize description
+     * @param {number} galaxyWidgetSettings.minPlanetCount description
+     * @param {number} galaxyWidgetSettings.maxPlanetCount description
+     * @param {number} galaxyWidgetSettings.minPlanetSize description
+     * @param {number} galaxyWidgetSettings.maxPlanetSize description
+     * @param {string} systemsJson A JSON string representing the systemsJson data
+     */
+    constructor(galaxyWidgetSettings = null, systemsJson = "") {
+        if (galaxyWidgetSettings === null && systemsJson === "") {
+            console.error("Galaxy cannot be constructed with a null galaxyWidgetSetting and an empty systemsJson string");
+            return;
         }
-        this.systems = this.unpackSystemsData(systemsData);
+
+        if (systemsJson === "") {
+            const systemsData = initializeNewGalaxy(galaxyWidgetSettings);
+            this.systems = this.unpackSystemsData(systemsData);
+            return;
+        }
+
+        if (galaxyWidgetSettings === null) {
+            const systemsData = loadGalaxyFromJson(systemsJson);
+            this.systems = this.unpackSystemsData(systemsData);
+            return;
+        }
+    }
+
+    initializeNewGalaxy(galaxyWidgetSettings) {
+        const systemsData = this.generateSystems(galaxyWidgetSettings);
+        const connections = this.generateConnections(systemsData);
+        this.addConnectionsToSystems(systemsData, connections);
+        return systemsData;
+    }
+
+    loadGalaxyFromJson(systemsJson) {
+        systemsData = JSON.parse(systemsJson);
     }
 
     unpackSystemsData(systemsData) {
-        const systems = []
+        const systems = [];
         for (const systemData of systemsData) {
             const system = new System(systemData);
             systems.push(system);
         }
         return systems;
-    }
-
-    replaceSystemsData(systemsJson) {
-        if (!systemsJson || systemsJson == '') {
-            alert('systems JSON not provided');
-        }
-        const systemsData = JSON.parse(systemsJson);
-        this.systems = this.unpackSystemsData(systemsData);
     }
 
     ///////////////////////////////
@@ -51,7 +76,7 @@ export class Galaxy {
         let starName = new StarName();
 
         // Define systems with coordinates
-        for (let systemIndex = 0; systemIndex < c.systemCount; systemIndex++){
+        for (let systemIndex = 0; systemIndex < c.systemCount; systemIndex++) {
             let position = this.generateSystemPosition(c.canvasWidth, c.canvasHeight, c.canvasBuffer);
             let i = 0;
             // Try maxPlacementAttempts times to find a system far enough away
@@ -78,16 +103,16 @@ export class Galaxy {
         let x = getRandomNum(canvasBuffer, canvasWidth - canvasBuffer, 0);
         let y = getRandomNum(canvasBuffer, canvasHeight - canvasBuffer, 0);
         let z = getRandomNum(-8, 8, 2);
-        return {x: x, y: y, z: z};
+        return { x: x, y: y, z: z };
     }
 
     isValidDistance(systemsData, position, systemBuffer) {
         // Checks the distance from the position to every existing system and
         // returns true if the position is far enough away
-        let isValid = systemsData.every( systemData => {
+        let isValid = systemsData.every(systemData => {
             let dist = Math.pow(
-                    Math.pow(systemData.position.x - position.x, 2) +
-                    Math.pow(systemData.position.y - position.y, 2),
+                Math.pow(systemData.position.x - position.x, 2) +
+                Math.pow(systemData.position.y - position.y, 2),
                 0.5);
             if (dist < systemBuffer) {
                 return false;
@@ -101,7 +126,7 @@ export class Galaxy {
     generateConnections(systemsData) {
         let connections = [];
         let connectedSystems = [];
-        while (connectedSystems.length < systemsData.length && systemsData.length > 1){
+        while (connectedSystems.length < systemsData.length && systemsData.length > 1) {
             let minDist = Infinity;
             let minI;
             let minJ;
@@ -112,11 +137,11 @@ export class Galaxy {
                     // If this is our first connection, or if i is connected while j is not, or vice versa
                     // then we should see if they're a minimal distance from each other so that they can be
                     // connected.
-                    if (        connections.length == 0
-                            || (connectedSystems.includes(i) && !connectedSystems.includes(j))
-                            || (connectedSystems.includes(j) && !connectedSystems.includes(i)) ) {
-                        let dist = (systemsData[i].position.x - systemsData[j].position.x)**2 +
-                            (systemsData[i].position.y - systemsData[j].position.y)**2;
+                    if (connections.length == 0
+                        || (connectedSystems.includes(i) && !connectedSystems.includes(j))
+                        || (connectedSystems.includes(j) && !connectedSystems.includes(i))) {
+                        let dist = (systemsData[i].position.x - systemsData[j].position.x) ** 2 +
+                            (systemsData[i].position.y - systemsData[j].position.y) ** 2;
                         if (dist < minDist) {
                             minDist = dist;
                             minI = i;
@@ -125,11 +150,11 @@ export class Galaxy {
                     };
                 };
             };
-            connections.push([minI,minJ])
-            if (!connectedSystems.includes(minI)){
+            connections.push([minI, minJ])
+            if (!connectedSystems.includes(minI)) {
                 connectedSystems.push(minI)
             }
-            if (!connectedSystems.includes(minJ)){
+            if (!connectedSystems.includes(minJ)) {
                 connectedSystems.push(minJ)
             }
         };
@@ -137,7 +162,7 @@ export class Galaxy {
     }
 
     addConnectionsToSystems(systemsData, connections) {
-        connections.forEach( connection => {
+        connections.forEach(connection => {
             const i = connection[0];
             const j = connection[1];
             this.addConnectionToSystem(systemsData, i, j);
@@ -147,9 +172,10 @@ export class Galaxy {
 
     addConnectionToSystem(systemsData, systemId, connectedSystemId) {
         const connectedSystemData = systemsData[connectedSystemId];
-        const connection = {id: connectedSystemData.id,
-                          name: connectedSystemData.name,
-                          position: connectedSystemData.position
+        const connection = {
+            id: connectedSystemData.id,
+            name: connectedSystemData.name,
+            position: connectedSystemData.position
         };
         systemsData[systemId].connections.push(connection);
     }
