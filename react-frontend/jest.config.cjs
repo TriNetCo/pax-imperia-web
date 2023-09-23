61209d7e6d2ce0861d856ed4a92a27c6ad5f64db
TheNotary <no@email.plz>
2023-09-22 22:47:43 -0500

    corrects vscode-jest configs such that react-frontend will test when opened directly (not from monorepo root)

--- react-frontend/jest.config.cjs
+++ react-frontend/jest.config.cjs
@@ -1,19 +1,11 @@
-// don't forget module name mapper configs
 const config = {
-//   rootDir: './../',
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tools/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/tools/styleMock.js'
-  },
-  testEnvironment: '<rootDir>/node_modules/jest-environment-jsdom',
-//   testEnvironment: 'jsdom',
-//   transform: {},
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-//   displayName: 'react-frontend',
-//   moduleDirectories: ['<rootDir>/node_modules', '<rootDir>'],
-  modulePaths: ['<rootDir>/node_modules', '<rootDir>'],
-//   modulePaths: ['<rootDir>/node_modules', '<rootDir>', '<rootDir>/../pax-imperia-js/node_modules'],
-//   testMatch: ['<rootDir>/**/*.test.js']
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tools/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/tools/styleMock.js'
+    },
+    testEnvironment: '<rootDir>/node_modules/jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    modulePaths: ['<rootDir>/node_modules', '<rootDir>'],
 };
 
 module.exports = config;
