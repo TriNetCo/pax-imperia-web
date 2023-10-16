571a22b1a34d97b7a7dfe0246fcf19dbf7d0c6c8
TheNotary <no@email.plz>
2023-10-15 20:43:04 -0500

    whitespace

--- react-frontend/src/App.test.js
+++ react-frontend/src/App.test.js
@@ -3,25 +3,25 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import Context from './app/Context';
 import { UserContextProvider } from './app/UserContextProvider';
 import FirebaseConnector from './app/FirebaseConnector';
 
 const azureAuth = {
-  initLoginContext: async () => { },
+    initLoginContext: async () => { },
 };
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <UserContextProvider azureAuth={azureAuth}>
-        <Router>
-          <App />
-        </Router>
-      </UserContextProvider>
-    </Provider>
-  );
+    const { getByText } = render(
+        <Provider store={store}>
+            <UserContextProvider azureAuth={azureAuth}>
+                <Router>
+                    <App />
+                </Router>
+            </UserContextProvider>
+        </Provider>
+    );
 
-  expect(getByText(/Pax Imperia/i)).toBeInTheDocument();
+    expect(getByText(/Pax Imperia/i)).toBeInTheDocument();
 });
