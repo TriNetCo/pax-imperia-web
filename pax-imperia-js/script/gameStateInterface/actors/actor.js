63930f9bbbc285be5a332c12ead9e2e4f9216ae5
savagedata <17114944+savagedata@users.noreply.github.com>
2023-10-29 14:47:19 -0500

    Add remove function for actor class

--- pax-imperia-js/script/gameStateInterface/actors/actor.js
+++ pax-imperia-js/script/gameStateInterface/actors/actor.js
@@ -11,4 +11,23 @@ export class Actor {
         this.websocket = websocket;
         this.gameStateInterface = gameStateInterface;
     }
+
+    remove(type, id) {
+        const galaxy = this.gameStateInterface.galaxy;
+        const entity = galaxy.getEntity(type, id);
+
+        galaxy.removeEntity(type, id);
+        this.removeFromScene(entity);
+    }
+
+    removeFromScene(entity) {
+        const spaceViewWidget = this.gameStateInterface?.spaceViewWidget;
+        // if player is currently viewing the system that the entity is in
+        if (entity.systemId === spaceViewWidget?.system?.id) {
+            const scene = spaceViewWidget.spaceViewAnimator.scene;
+            scene.remove(entity.object3d);
+            spaceViewWidget.spaceViewDomManager.populateHtml();
+        }
+    }
+
 }
