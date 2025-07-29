b4f620862b4909946454b471bef84714cfae69ff
TheNotary <no@email.plz>
2025-07-29 13:34:56 -0500

    frontend: removes test for counterSlice which isn't used in the app

--- react-frontend/src/app/store.js
+++ react-frontend/src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/Counter/counterSlice';
 import usersReducer from '../features/Users/usersSlice';
 import lobbiesReducer from '../features/Lobbies/lobbiesSlice';
 import socketMiddleware from '../modules/socketMiddleware';
@@ -11,7 +10,6 @@ const websocketFactory = (host) => {
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         users: usersReducer,
         lobbies: lobbiesReducer,
         websocket: websocketReducer
